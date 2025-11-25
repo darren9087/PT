5fd5a8aa6e9d5aa7d7382023121289267a65874e
Eng <Eng@AHREN>
2025-11-25 15:20:45 +0800

    Add project files.

---
+++ PT/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PT
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PT/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PT")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PT.Pages")]
